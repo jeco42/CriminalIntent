f9cf5fbab170d435ce3a6a7b84333dd181eacbb1
jeco42 <jeco42@gmail.com>
2015-02-04 00:16:25 -0500

    Initial commit

---
+++ app/src/main/java/com/weebley/jeco42/criminalintent/Crime.java
@@ -0,0 +1,49 @@
+package com.weebley.jeco42.criminalintent;
+
+import java.util.UUID;
+import java.util.Date;
+
+/**
+ * Created by Justin on 2/3/2015.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+
+            public Crime() {
+                // Generate unique identifier
+                mId = UUID.randomUUID();
+                setDate(new Date());
+        }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
